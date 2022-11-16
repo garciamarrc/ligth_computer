f33e9a791be817969c38334e85196acead079890
marcogarz <dev.84ui7@aleeas.com>
2022-11-16 10:30:55 -0600

    Mostrando categorias en home

--- public_html/views/Home/index.php
+++ public_html/views/Home/index.php
@@ -6,20 +6,25 @@ define('HEADER_TITLE', 'Home');
 include View::layout('header.layout');
 include View::component('navbar');
 ?>
-<table>
-    <thead>
-        <th>Nombre</th>
-        <th>Clasificación hija</th>
-    </thead>
-    <tbody>
+
+<div class="container">
+    <h1>Bienvenido de nuevo!</h1>
+
+    <hr>
+
+    <div class="row gap-3">
+        <h2>Categorías</h2>
         <?php foreach ($classifications as $classification) : ?>
-            <tr>
-                <td><?= $classification->getName(); ?></td>
-                <td><?= $classification->getSubClassification(); ?></td>
-            </tr>
+            <div class="col-sm-12 col-md-6 col-lg-4 mx-auto">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title"><?= $classification->getName(); ?></h3>
+                    </div>
+                </div>
+            </div>
         <?php endforeach; ?>
-    </tbody>
-</table>
+    </div>
+</div>
 <?php
 include View::layout('tail.layout');
 ?>
