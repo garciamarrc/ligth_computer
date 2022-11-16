f3ab5b8235b2d539b649ccbf214a794cf48c73d7
marcogarz <dev.84ui7@aleeas.com>
2022-11-16 12:24:23 -0600

    Texto de ayuda en caso de que no se hayan encontrado productos finalizado

--- public_html/views/Classification/show.php
+++ public_html/views/Classification/show.php
@@ -14,17 +14,21 @@ include View::component('navbar');
 
     <div class="row gap-3 my-4">
         <h2>Productos destacados - <?= $classification->getName() . '/' . $classification->getSubClassification(); ?></h2>
-        <?php foreach ($products as $product) : ?>
-            <div class="col-sm-12 col-md-4 col-lg-4 mx-auto">
-                <div class="card">
-                    <div class="card-body">
-                        <h3 class="card-title"><?= $product->getModel(); ?></h3>
-                        <p class="card-text text-muted"><?= $product->getSpecs(); ?></p>
-                        <p class="card-text"><strong>$<?= number_format($product->getPrice(), 2); ?></strong></p>
+        <?php if (count($products) === 0) : ?>
+            <h3 class="text-muted">No se encontraron productos</h3>
+        <?php else : ?>
+            <?php foreach ($products as $product) : ?>
+                <div class="col-sm-12 col-md-4 col-lg-4 mx-auto">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="card-title"><?= $product->getModel(); ?></h3>
+                            <p class="card-text text-muted"><?= $product->getSpecs(); ?></p>
+                            <p class="card-text"><strong>$<?= number_format($product->getPrice(), 2); ?></strong></p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        <?php endif; ?>
     </div>
 
 </div>
