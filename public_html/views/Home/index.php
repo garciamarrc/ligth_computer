2d3bef0e38c496a4f62002ad819b79f021f7ce51
marcogarz <dev.84ui7@aleeas.com>
2022-11-16 10:57:42 -0600

    Acorddion añadido para mostrar las categorias

--- public_html/views/Home/index.php
+++ public_html/views/Home/index.php
@@ -12,17 +12,25 @@ include View::component('navbar');
 
     <hr>
 
-    <div class="row gap-3 my-4">
-        <h2>Categorías</h2>
-        <?php foreach ($classifications as $classification) : ?>
-            <div class="col-sm-12 col-md-6 col-lg-4 mx-auto">
-                <div class="card">
-                    <div class="card-body">
-                        <h3 class="card-title"><?= $classification->getName(); ?></h3>
-                    </div>
+    <div class="accordion accordion-flush" id="categories-accordion">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="categories-flush-heading">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#categories-flush-collapse" aria-expanded="true" aria-controls="categories-flush-collapse">
+                    Categorías
+                </button>
+            </h2>
+            <div id="categories-flush-collapse" class="accordion-collapse collapse" aria-labelledby="categories-flush-heading" data-bs-parent="#categories-accordion">
+                <div class="accordion-body">
+                    <ul>
+                        <?php foreach ($classifications as $classification) : ?>
+                            <li>
+                                <span class="btn"><u><?= $classification->getName(); ?></u></span>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
                 </div>
             </div>
-        <?php endforeach; ?>
+        </div>
     </div>
 
     <div class="row gap-3 my-4">
