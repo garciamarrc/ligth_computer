01e9deeea917b8efece1cd83ace2d9517c45df05
marcogarz <dev.84ui7@aleeas.com>
2022-11-16 12:09:49 -0600

    Clasificaciones y subclasificaciones mostradas en home

--- php/Controllers/HomeController.php
+++ php/Controllers/HomeController.php
@@ -11,6 +11,8 @@ class HomeController
   public function index()
   {
     $classifications = Classification::readQuery("SELECT * FROM clasificacion GROUP BY nombre");
+    $sub_classifications = Classification::readQuery("SELECT * FROM clasificacion");
+    
     $products = Product::readQuery("SELECT * FROM productos ORDER BY RAND() LIMIT 10");
 
     include View::view('Home/index');
