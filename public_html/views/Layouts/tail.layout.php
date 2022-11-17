572a265b4f79c7903c76b9477c534851ba85e69a
marcogarz <dev.84ui7@aleeas.com>
2022-11-17 17:10:35 -0600

    footer añadido a tail

--- public_html/views/Layouts/tail.layout.php
+++ public_html/views/Layouts/tail.layout.php
@@ -1,3 +1,10 @@
+<footer class="text-center text-lg-start my-4">
+    <div class="text-center p-3">
+        © <?= date('Y'); ?> Copyright:
+        <a class="text-dark" href="https://marcogarz.netlify.app/" target="_blank">Marco Garcia</a>
+    </div>
+</footer>
+
 </body>
 
 </html>
