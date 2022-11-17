675fd5f2fb5c596cb496f67da28c1a0d20f108cf
marcogarz <dev.84ui7@aleeas.com>
2022-11-17 16:02:15 -0600

    Componente alternativo de productos eliminado. Listado de todas las secciones con sus productos mas vendidos añadida

--- public_html/views/Home/index.php
+++ public_html/views/Home/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Helpers\View;
+use App\Models\Product;
 
 define('HEADER_TITLE', 'Home');
 include View::layout('header.layout');
@@ -24,20 +25,37 @@ include View::component('navbar');
             </div>
         </div>
     </div>
+
     <div class="row">
         <div class="col-sm-12 gap-3 my-4">
             <h2>Productos más vendidos de cada clasificación</h2>
             <div class="row align-items-start">
                 <?php
-                foreach ($most_sell_products_by_sub_category as $most_sell_products_by_sub_category) {
-                    $most_sell_product_title;
+                foreach ($sub_classifications as $sub_classification) {
+                    $products = Product::readQuery("SELECT * FROM productos WHERE id_clasificacion = '{$sub_classification->getId()}' ORDER BY ventas DESC LIMIT 20");
 
-                    foreach ($sub_classifications as $sub_classification) {
-                        if ($sub_classification->getId() === $most_sell_products_by_sub_category->getIdClassification()) {
-                            $most_sell_product_title = $sub_classification->getName() . ' - ' . $sub_classification->getSubClassification();
-                        }
+                    $classification_title = $sub_classification->getName() . ' - ' . $sub_classification->getSubClassification();
+
+                    $classification_id_html = str_replace(' ', '', $classification_title);
+
+                    echo "<div class='accordion accordion-flush' id='accordion-flush-{$classification_id_html}'>
+                    <div class='accordion-item'>
+                      <h2 class='accordion-header' id='flush-heading-{$classification_id_html}'>
+                        <span class='accordion-button collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#flush-collapse-{$classification_id_html}' aria-expanded='true' aria-controls='flush-collapse-{$classification_id_html}'>
+                          {$classification_title}
+                        </span>
+                      </h2>
+                      <div id='flush-collapse-{$classification_id_html}' class='accordion-collapse collapse' aria-labelledby='flush-heading-{$classification_id_html}' data-bs-parent='#accordion-flush-{$classification_id_html}'>
+                        <div class='accordion-body row'>";
+
+                    foreach ($products as $product) {
+                        include View::component('product.card');
                     }
-                    include View::component('most_sell_product.card');
+
+                    echo "</div>
+                        </div>
+                    </div>
+                </div>";
                 }
                 ?>
             </div>
