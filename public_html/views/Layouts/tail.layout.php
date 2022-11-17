3012f436a067fcbe1428096e1d0a54a8b1e2265b
marcogarz <dev.84ui7@aleeas.com>
2022-11-17 17:11:44 -0600

    Palabra copyrigth eliminada por falta de sentido

--- public_html/views/Layouts/tail.layout.php
+++ public_html/views/Layouts/tail.layout.php
@@ -1,6 +1,6 @@
 <footer class="text-center text-lg-start my-4">
     <div class="text-center p-3">
-        © <?= date('Y'); ?> Copyright:
+        © <?= date('Y'); ?> - 
         <a class="text-dark" href="https://marcogarz.netlify.app/" target="_blank">Marco Garcia</a>
     </div>
 </footer>
