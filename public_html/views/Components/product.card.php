5e69db273181fc52f725249fe0c776e7352ca6c6
marcogarz <dev.84ui7@aleeas.com>
2022-11-17 12:59:55 -0600

    Card style changed

--- public_html/views/Components/product.card.php
+++ public_html/views/Components/product.card.php
@@ -1,11 +1,15 @@
+<?php
+
+use App\Helpers\Asset;
+?>
 <div class="col-sm-12 col-lg-4 mx-auto my-2">
-    <a href="<?= APP_URL . 'product/show/' . $product->getId(); ?>" style="text-decoration: none;">
-    <div class="card">
+    <div class="card" style="width: 18rem;">
+        <img src="<?= APP_URL ?>public_html/assets/img/ligth-logo.png" class="card-img-top" alt="...">
         <div class="card-body">
-                <h3 class="card-title"><?= $product->getModel(); ?></h3>
-                <p class="card-text text-muted"><?= substr($product->getSpecs(), 0, 59); ?>...</p>
-                <p class="card-text"><strong>$<?= number_format($product->getPrice(), 2); ?></strong></p>
-            </div>
+            <h5 class="card-title"><?= $product->getModel(); ?></h5>
+            <p class="card-text text-muted"><?= substr($product->getSpecs(), 0, 59); ?>...</p>
+            <p class="card-text"><strong>$<?= number_format($product->getPrice(), 2); ?></strong></p>
+            <a href="<?= APP_URL . 'product/show/' . $product->getId(); ?>" class="btn btn-primary">Visitar producto</a>
         </div>
-    </a>
+    </div>
 </div>
